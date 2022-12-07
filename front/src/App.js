2e11aca7b8c5945158c569cd9fd503116bad7ffc
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-12-07 11:52:35 +0900

    init CRA, eslint

---
+++ front/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="h-full bg-blue-500">div App</div>;
+}
+
+export default App;
---
+++ front/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [],
+  content: ['./src/**/*.js', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
