79798a7ecdfe10abfbc4871e3aa41d5a6eafb859
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-12-19 19:44:06 +0900

    Design: show current Ip

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -4,7 +4,7 @@ import useHeader from '../hooks/useComponents/useHeader';
 import SetIpPopup from './SetIpPopup';
 
 function Header() {
-  const { modalPopup, setModalPopup } = useHeader();
+  const { modalPopup, setModalPopup, currentUserIp } = useHeader();
   return (
     <nav className=" px-2 py-2.5 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
@@ -31,18 +31,19 @@ function Header() {
           </Link>
         </div>
 
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <div className="mt-4 rounded-lg border border-gray-700 duration-500 hover:bg-gray-700">
+        <div className=" w-full md:block md:w-auto" id="navbar-default">
+          <div className=" mt-4 rounded-lg border border-gray-700  duration-500 hover:bg-gray-700">
             <button
               type="button"
               onClick={() => {
                 setModalPopup(true);
               }}
-              className="block rounded py-2 pl-3 pr-4 text-gray-700 hover:text-gray-100"
+              className=" mx-auto block rounded py-2 pl-3 pr-4 text-gray-700 hover:text-gray-100"
             >
               내 IP 설정하기
             </button>
           </div>
+          <p>{`현재 내 ip 주소 : ${currentUserIp}`}</p>
         </div>
       </div>
       {modalPopup && <SetIpPopup setModalPopup={setModalPopup} />}
